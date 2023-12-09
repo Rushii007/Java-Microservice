023baa632a366c2ead759fd7965ab72a7d159c96
Rushii007 <ru.chavan@rabbitandtortoise.com>
2023-12-09 18:33:47 +0530

    commit for fault tolarance

--- User-Service/src/main/java/com/rushi/microservice/user/controller/UserController.java
+++ User-Service/src/main/java/com/rushi/microservice/user/controller/UserController.java
@@ -5,6 +5,8 @@ package com.rushi.microservice.user.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rushi.microservice.user.entities.User;
 import com.rushi.microservice.user.services.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 /**
  * 
  */
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	public static final Logger log = LogManager.getLogger(UserController.class); 
+	
+	Integer count = 1;
 	
 	@Autowired
 	private UserService userService;
@@ -35,9 +45,32 @@ public class UserController {
 	} 
 	
 	@GetMapping("/{userId}")
+	//if service is down then use below annotation
+//	@CircuitBreaker(name = "userBreaker",fallbackMethod =  "userFallbackFun")
+
+	//if service is slow then use below annotation
+//	@Retry(name = "userRetryService",fallbackMethod =  "userFallbackFun")
+	//if client try to hit same req for multiple time using below annotation we can prevent this
+	@RateLimiter(name = "userLimiterService",fallbackMethod =  "userFallbackFun")
 	public ResponseEntity<User> getUser(@PathVariable String userId) {
+		log.info("Retry count: {}",count);
+		count++;
 		User user = userService.getUserById(userId);
 		return ResponseEntity.ok(user);
+	}
+	
+	public ResponseEntity<User> userFallbackFun(String userId, Exception e) {
+		log.error("Fallback is executed because service is down: {}", e.getMessage());
+		e.printStackTrace();
+		
+		User user = User.builder().name("Temp Name")
+				.email("temp@gmail.com")
+				.about("This method is calling because some service is down")
+				.userId("123456789")
+				.build();
+		
+		return new ResponseEntity<>(user, HttpStatus.OK);
+		
 	}
 	
 	@GetMapping
